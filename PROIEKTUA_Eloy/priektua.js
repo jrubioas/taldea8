ca9df2f388a635d0516f6aea071981028f8e89a7
Eloy_Garcia <egarciamo21ba@ikzubirimanteo.com>
2023-03-15 13:06:42 +0100

    no message

--- PROIEKTUA_Eloy/priektua.js
+++ PROIEKTUA_Eloy/priektua.js
@@ -14,6 +14,6 @@ function U(){
   location.assign("file:///C:/Users/EloyGarc%C3%ADaMor%C3%A1n/Desktop/js/PROIEKTUA_Eloy/perfil_.html")
 }
 async function ekarriOrdua() {
-  var data = await (await fetch('http://zubiriapi.duckdns.org:8000/users/taldea8')).json()
+  var data = await (await fetch('http://zubiriapi.duckdns.org:8000/select/select * from users/taldea8')).json()
   document.getElementById("ordua").innerHTML= data.item_id;
 }
