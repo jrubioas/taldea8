e0b2fdbacbd9dd8241d65e462120631ac516a597
Eloy_Garcia <egarciamo21ba@ikzubirimanteo.com>
2023-03-27 08:02:58 +0200

    no message

--- PROIEKTUA_Eloy/priektua.js
+++ PROIEKTUA_Eloy/priektua.js
@@ -1,4 +1,7 @@
-
+function recibir(){
+  const p = url_img;
+  console.log(p.replace('-', '/'));
+}
 function openNav() {
   if (document.getElementById("main").style.marginLeft == "0px" || document.getElementById("main").style.marginLeft == "") {
     document.getElementById("btn_open").innerHTML = "X";
@@ -11,9 +14,18 @@ function openNav() {
   }
 }
 function U(){
-  location.assign("file:///C:/Users/EloyGarc%C3%ADaMor%C3%A1n/Desktop/js/PROIEKTUA_Eloy/perfil_.html")
+  location.assign("file:///C:/Users/EloyGarc%C3%ADaMor%C3%A1n/Desktop/Git/PROIEKTUA_Eloy/editperfil_.html")
 }
-async function ekarriOrdua() {
-  var data = await (await fetch('http://zubiriapi.duckdns.org:8000/select/select * from users/taldea8')).json()
-  document.getElementById("ordua").innerHTML= data.item_id;
+async function bidali() {
+  var url_img = "https://images.musement.com/cover/0002/52/thumb_151722_cover_header.jpeg"
+  
+  const p = url_img;
+  console.log(p.replace('/', '-'));
+  
+  var Eloy = await (await fetch("http://zubiriapi.duckdns.org:8000/insert/insert into post values ('1234','0','1',"+url_img+",'2222')/taldea8")).json();
+  document.getElementById("demasiado").innerHTML= Eloy.item_id;
 }
+var loadFile = function(event) {
+	var image = document.getElementById('output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+};
