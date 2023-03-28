8205d5bde4e88e9c622cd2825ebb99f64d5ae55e
Lander_Esnal <lesnalal21ba@ikzubirimanteo.com>
2023-03-28 12:23:10 +0200

    no message

--- PROIEKTUA_LANDER/ideamillonaria.js
+++ PROIEKTUA_LANDER/ideamillonaria.js
@@ -9,12 +9,18 @@ function openNav() {
     document.getElementById("main").style.marginLeft = 0;
   }
 }
-
+const fotos = [ "output1","output2","output3","output4","output5","output6"]
+i = 0;
 var loadFile = function(event) {
-	var image = document.getElementById('output');
+  var image = document.getElementById(fotos[i]);
 	image.src = URL.createObjectURL(event.target.files[0]);
+  i = i+1;
 };
 
 async function masunolike(post) {
  //update post set post_like = post_like + 1 where post_id = post
+}
+
+function U(){
+  location.assign("file:///C:/Users/LanderEsnalAlzugaray/Desktop/GIT/PROIEKTUA_LANDER/editperfil.html")
 }
